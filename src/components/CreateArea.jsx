851821a7ada75d52735663d89678e48cefed13fc
saltanat@pingocean.com <saltanat@pingocean.com>
2020-03-01 18:29:47 +0600

    front end google keep analoge

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
+import Zoom from '@material-ui/core/Zoom';
 
-function CreateArea() {
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+  
+  const [isExpanded, setExpand] = useState(false);
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote(prevNote => {
+      return {
+        ...prevNote,
+        [name]: value
+      };
+    });
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: ""
+    });
+    event.preventDefault();
+  }
+
+  function hanleExpandInput() {
+    setExpand(true)
+
+  }
+
   return (
     <div>
-      <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+      <form className="create-note">
+        { isExpanded && <input
+          name="title"
+          onChange={handleChange}
+          value={note.title}
+          placeholder="Title"
+        /> }
+
+        <textarea
+          onClick={hanleExpandInput}
+          name="content"
+          onChange={handleChange}
+          value={note.content}
+          placeholder="Take a note..."
+          rows={isExpanded}
+        />
+        <Zoom in={true}>
+        <Fab onClick={submitNote}><AddIcon/></Fab >
+        </Zoom>
       </form>
     </div>
   );
